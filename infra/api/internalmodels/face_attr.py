4087ed153ba0f17b0257490266b3023774e9e683
macbook <macbook@melon.local>
2024-09-24 12:03:56 +0900

    added new internal models

---
+++ infra/api/internalmodels/face_attr.py
@@ -0,0 +1,112 @@
+import os
+import cv2
+import math
+import numpy as np
+import concurrent.futures
+import onnxruntime as ort
+from infra.common.fd_utils import letterbox
+from domain.interface import result, Error, point
+from infra.api.internalmodels.face_detect import loosedetectxyxy
+from infra.common.utils import data_uri_to_cv2_img, mean_image_subtraction
+import josephlogging.log as log
+
+DEBUG = False
+
+logger = log.getLogger(__name__)
+providers =  ['CPUExecutionProvider']
+ort_session = {}
+emotion_session = None
+attribute_task = {
+    'hat': 'hat_detection.onnx'
+}
+
+hat_class = {0: 'No Hat', 1: 'Hat', 2: 'Hat'}
+
+def init(task:str, modelname:str):
+    try:
+        if task == 'emotion':
+            global emotion_session
+            emotion_session = ort.InferenceSession(modelname, providers=providers)
+            return True
+        global ort_session
+        ort_session[task] = ort.InferenceSession(modelname, providers=providers)
+        return True
+    except Exception as e:
+        logger.error(f'Error initializing model: {e}')
+        return False
+
+def get_attributes(img):
+    # set orts
+    for k,v in attribute_task.items():
+        file_path = os.path.join('infra', 'model', v)
+        if k not in ort_session:
+            if not init(k, file_path):
+                return Error(f'FA Model {file_path}', 'initialization failed')
+    # preprocess
+    img224 = cv2.resize(img,(224,224))
+    img = letterbox(img,new_shape=64, auto=False)[0]
+    
+    n_img = mean_image_subtraction(img/255)
+    n_img = n_img.transpose(2, 0, 1).copy() #(64,64,3)BGR to (3,64,64)BGR
+    data = np.expand_dims(n_img, axis=0).astype(np.float32) #(1,3,64,64)
+    res = {}
+    # inference
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+    # inference
+        futures = {k: executor.submit(run_session, session, data) for k, session in ort_session.items()}
+        for k, future in futures.items():
+            res[k] = postprocess(k, future.result())
+
+    return res
+
+def run_session(session, data):
+    in_name = session.get_inputs()[0].name
+    out_name = [session.get_outputs()[x].name for x in range(len(session.get_outputs()))]
+    output = session.run(out_name, {in_name: data})
+    raw_score = output[0][0]
+    return raw_score
+
+def predict(base64_image: str): 
+    rgb_image = data_uri_to_cv2_img(logger, base64_image)
+    bbs = loosedetectxyxy(img=rgb_image, fx=1.3)
+    res_bbs = []
+    ms = []
+    for bb in bbs:
+        img = rgb_image[bb[1]:bb[3],bb[0]:bb[2],:]
+        res = get_attributes(img)
+        ms.append(str(res))
+        tup = (point(x=bb[0],y=bb[1]), point(x=bb[2],y=bb[3]))
+        res_bbs.append(tup)
+    output = {'detected faces': len(bbs), 'attributes':ms}
+    if DEBUG: cv2.imwrite('letout.jpg', img)   
+    return result(name='attributes', bb=res_bbs ,imgtxt=ms, outstr=output)
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+def postprocess(task:str, score: float):
+    if task == 'hat':
+        res = softmax(score)
+        res_class = np.argmax(res)
+        ret_hat = hat_class[res_class]
+        return ret_hat
+    else:
+        return Error(what='unsupported attribute', where='face_attr.py')
+
+def softmax(x, axis=None):
+    x_max = np.amax(x, axis=axis, keepdims=True)
+    exp_x_shifted = np.exp(x - x_max)
+    return exp_x_shifted / np.sum(exp_x_shifted, axis=axis, keepdims=True)
+
+if __name__ == '__main__':
+    img = cv2.imread("/Users/macbook/Code/MVP/JosephPlatform/test_img/0.jpg")
+    try:
+        bbs = loosedetectxyxy(img=img, fx=1.5)
+        bb = bbs[0]
+        img2 = img[bb[1]:bb[3],bb[0]:bb[2],:]
+    except:
+        img2 = img
+    res = get_attributes(img2)
+    print(res)
+    
+    
---
+++ infra/api/internalmodels/face_detect.py
@@ -0,0 +1,173 @@
+import os
+import cv2
+import time
+import torch
+import base64
+import numpy as np
+import onnxruntime as ort
+from infra.common.fd_utils import cfg_blaze, PriorBox, decode, letterbox, decode_landm, py_cpu_nms
+from infra.common.utils import data_uri_to_cv2_img
+from domain.interface import result, point, Error
+import josephlogging.log as log
+
+DEBUG = False
+logger = log.getLogger(__name__)
+providers =  ['CPUExecutionProvider']
+ort_session = None
+priorbox = PriorBox(cfg_blaze, image_size=(256, 256))
+priors = priorbox.forward()
+
+def init(modelname):
+    try:
+        global ort_session
+        ort_session = ort.InferenceSession(modelname, providers=providers)
+        return True
+    except Exception as e:
+        logger.error(f'Error initializing model: {e}')
+        return False
+
+def softmax(x, axis=None):
+    x_max = np.amax(x, axis=axis, keepdims=True)
+    exp_x_shifted = np.exp(x - x_max)
+    return exp_x_shifted / np.sum(exp_x_shifted, axis=axis, keepdims=True)
+
+def predict(image):
+    img_raw = data_uri_to_cv2_img(logger, image)
+    if img_raw is None:
+        return Error(what='decoding error', where='base64 image')
+    return inference(img_raw)
+
+def detectxyxy(img):
+    fd = inference(img)
+    bbs = fd.bounding_box
+    ret = [[bb[0].x, bb[0].y, bb[1].x, bb[1].y] for bb in bbs]
+    return ret
+
+def loosedetectxyxy(img, fx):
+    bbs = detectxyxy(img)
+    h,w = img.shape[:2]
+    new_det = []
+    for det in bbs:
+        cw,ch = (det[0]+det[2])//2, (det[1]+det[3])//2
+        dw,dh = (det[2]-det[0])*fx//2, (det[3]-det[1])*fx//2
+        new_bb =  [max(0,cw-dw), max(0,ch-dh), min(w,cw+dw), min(h,ch+dh)]
+        new_det.append([int(x) for x in new_bb])
+    return new_det
+
+def inference(img_raw, vis_thres=0.6):
+    st0 = time.time()
+    outlog = {}
+    img_size = 256
+    nms_thresh = 0.6
+
+    f = os.path.join('infra', 'model', 'face_detect.onnx')
+    if ort_session is None:
+        if not init(f):
+            return Error(f'FD Model {f}', 'initialization failed')
+
+    img0 = np.uint8(img_raw)
+    img = letterbox(img0, color=(104, 117, 123), new_shape=img_size, auto=False)[0]
+    
+    img = img.transpose(2, 0, 1).copy() #(256,256,3)BGR to (3,256,256)BGR
+    data = np.expand_dims(img, axis=0).astype(np.float32) #(1,3,256,256)
+    in_name = ort_session.get_inputs()[0].name # 'input'
+    out_name = [ort_session.get_outputs()[x].name for x in range(len(ort_session.get_outputs()))] # 'output'
+
+    t0 = time.time()
+    loc, conf, landmk = ort_session.run(out_name, {in_name: data})
+    outlog['speed'] = ('took {}'.format(time.time() - t0))
+    
+    cfg = cfg_blaze
+    prior_data = priors.data
+    boxes = decode(torch.tensor(loc.squeeze()), prior_data, cfg['variance'])
+    landms = decode_landm(torch.tensor(landmk.squeeze()), prior_data, cfg['variance'], num_landmarks=106)
+    conf = softmax(conf, axis=2)
+    scores = conf.squeeze()[:, 1]
+    # ignore low scores
+    inds = np.where(scores > nms_thresh)[0]
+    boxes = boxes[inds]
+    landms = landms[inds]
+    scores = scores[inds]
+    
+    img = np.float32(img_raw)#[height, width, 3]
+    img = img.transpose(2, 0, 1)#[3, height, width]
+    img = torch.from_numpy(img).unsqueeze(0)#[1, 3, height, width]
+
+    r = max(img.shape[2],img.shape[3])
+    off_y = (r - img.shape[2])//2
+    off_x = (r - img.shape[3])//2
+    #resize back
+    scale = torch.Tensor([r]*4)
+    boxes = boxes * scale - torch.Tensor([off_x, off_y] * 2) # (height, width)
+    boxes = boxes.cpu().numpy()
+
+    scale = torch.Tensor([r]*212)
+
+    landms = landms * scale - torch.Tensor([off_x, off_y] * 106) # (x, y)
+    landms = landms.cpu().numpy()
+
+    # keep top-K before NMS
+    order = scores.argsort()[::-1][:5000]#args.topk = 5000
+    boxes = boxes[order]
+    landms = landms[order]
+    scores = scores[order]
+
+    # do NMS
+    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+    keep = py_cpu_nms(dets, nms_thresh) #args.nms_threshold = 0.4
+    # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+    dets = dets[keep, :]
+    landms = landms[keep]
+
+    # keep top-K faster NMS
+    keep_top_k = 750 #args keep_top_k
+    dets = dets[:keep_top_k, :]
+    landms = landms[:keep_top_k, :]
+
+    bbs = []
+    lmks = []
+    txt = []
+    scores = []
+    dets = np.concatenate((dets, landms), axis=1)
+    # show image
+    for b in dets:
+        if b[4] < vis_thres:
+            continue
+        text = "(({},{}),({},{})) Confidence: {:.4f}".format(int(b[0]), int(b[1]), int(b[2]), int(b[3]), b[4])
+        x1,y1,x2,y2 = int(b[0]),int(b[1]),int(b[2]),int(b[3])
+        bbs.append((point(x1,y1),point(x2,y2)))
+        lmks.append([point(b[5+i*2],b[6+i*2]) for i in range(106)])
+        txt.append(text)
+        scores.append(float(b[4]))
+
+        if DEBUG:
+            b = list(map(int, b))
+            cv2.rectangle(img_raw, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            if 'bbox' not in outlog:
+                outlog['bbox'] = ''
+            outlog['bbox'] += (f'{x1}, {y1}, {x2}, {y2}\n')
+            cx = x1
+            cy = y1 - 12
+            cv2.putText(img_raw, text, (cx, cy),
+                        cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+            # landms
+            for i in range(106):
+                cv2.circle(img_raw, (b[5+i*2], b[6+i*2]), 1, (0, 0, 255), 2)
+    
+    # save image
+    if DEBUG:
+        cv2.imwrite('output.jpg', img_raw)
+    outlog['outcome'] = txt
+    res = result(name='face_detect', bb=bbs, lms=lmks, scores=scores, imgtxt=txt, outstr=outlog)
+    return res
+
+if __name__ == '__main__':
+    with open("test_img/0.jpg", "rb") as image_file:
+        encode = base64.b64encode(image_file.read()).decode('utf-8')
+    st = time.time()
+    bb = predict(encode).bounding_box[0]
+    print(bb[0].to_str(), bb[1].to_str())
+    print(f'average took: {(time.time() - st)} s')
+    # img = cv2.rectangle(cv2.imread("test_img/0.jpg"), (bb[0].x, bb[0].y), (bb[1].x, bb[1].y), (0, 0, 255), 2)
+    # cv2.imwrite('output.jpg', img)

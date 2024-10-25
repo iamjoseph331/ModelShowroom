3005d7cfed89a4f1a30c2a37cb29ba3818070b26
macbook <macbook@Spinach.local>
2024-10-25 17:13:27 +0900

    added datallogs

--- infra/common/datacollection.py
+++ infra/common/datacollection.py
@@ -154,11 +154,9 @@ def upload_frame(timestamp:int, img:str, task:str, model:str, metadata:str, scor
     name = name + f'.{ext}' 
     fullname = fullname + f'.{ext}'
     path = os.path.join(os.getcwd(), 'infra', 'common', 'datalogs', name)
-    print(path, img2)
     try:
         with open(path, 'wb+') as fh:
             fh.write(base64.decodebytes(bytes(img2, 'utf-8')))
-        logger.info(f"Image saved locally at: {path}")
         # Initiate asynchronous upload
         future = upload_async(localpath=path, filename=fullname, metadata=metadata)
         #logger.info(f"Started asynchronous upload for file: {fullname}")
